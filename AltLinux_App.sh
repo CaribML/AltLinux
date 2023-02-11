2bf760f5edf1380370da9f78397d1063fe733fa8
CaribML <68754969+CaribML@users.noreply.github.com>
2023-02-11 16:26:34 +0300

    Update AltLinux_App.sh

--- AltLinux_App.sh
+++ AltLinux_App.sh
@@ -1,1 +1,20 @@
-#!bin/bash
+#!/bin/bash
+	echo "Author: Sudaev Andrey"
+	echo "Apps AltLinux 10.1"
+
+	# Sys env / paths / etc
+	# -------------------------------------------------------------------------------------------\
+ apt-get install -y gnome-disk-utility && 
+ apt-get install -y evolution && 
+ apt-get install -y evolution-ews && 
+ epm play yandex-browser && 
+ epm play chrome && 
+ epmi polkit-rule-udisks2-mount && 
+ # epm play zoom && epm play telegram && epm play anydesk && 
+ epm play yandex-disk &&
+ epm play xnview &&
+ apt-get install -y fonts-ttf-ms &&
+ apt-get install neofetch &&
+ apt-get update &&
+ neofetch
+ echo "Установка успешно завершена! С уважением, коллектив ОТП РОСМОРПОРТ"
